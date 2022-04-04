903c2e26136af6e9c650b69f420a7ce76f0d391b
Denzel <dbartolaba123@gmail.com>
2022-04-04 11:02:45 -0700

    got 2nd and 3rd problem finished

--- sandbox.js
+++ sandbox.js
@@ -4,3 +4,23 @@ function reverseWords(sentence) {
 }
 
 console.log(reverseWords('alchemy rocks gold'));
+
+
+function titleCase(sentence) {
+  const splitSentence = sentence.split(' ');
+  return splitSentence.map((word) => (word[0]).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+}
+
+console.log(titleCase('alchemy ROCKS goLD'));
+
+function oddishOrEvenish(number) {
+  const stringArray = number.toString().split('');
+  const numArray = stringArray.map(Number);
+  const sum = numArray.reduce(
+    (prevValue, currentValue) => prevValue + currentValue,
+    0
+  );
+  return sum % 2 === 0 ? 'Evenish' : 'Oddish';
+}
+
+console.log(oddishOrEvenish(121));
